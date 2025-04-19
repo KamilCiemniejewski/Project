d3c9bee6342a1b901121a6e9b2528fcac07b9c36
unknown <G00433270@atu.ie>
2025-04-19 12:47:33 +0000

    Allow user to search for cities

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput } from '@ionic/angular/standalone';
 import { WeatherService } from '../services/weather.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput],
 })
 export class HomePage implements OnInit {
 
@@ -16,8 +17,13 @@ city: string = "Galway";
 temperature: string = "";
 
   constructor(private weatherService:WeatherService) {}
-
   ngOnInit(): void {
+    this.getWeather(); // Load default city on init
+  }
+
+  getWeather(): void {
+    if (!this.city || this.city.trim().length === 0) return;
+
     this.weatherService.getWeatherData(this.city).subscribe((data) => {
       this.weatherData = data;
       this.temperature = data.main.temp.toFixed(2);
