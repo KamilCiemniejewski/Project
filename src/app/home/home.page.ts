0c251669dc1d3374ccc8e9319ed84655e7444abe
unknown <G00433270@atu.ie>
2025-04-22 18:18:33 +0000

    Data Storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput, IonBu
 import { WeatherService } from '../services/weather.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
@@ -16,9 +17,24 @@ weatherData: any;
 city: string = "Galway";
 temperature: string = "";
 
-  constructor(private weatherService:WeatherService) {}
-  ngOnInit(): void {
-    this.getWeather(); // Load default city on init
+  constructor(private weatherService:WeatherService, private storage: Storage) {}
+  
+  async ngOnInit() {
+    await this.storage.create();
+    const storedCity = await this.storage.get('city');
+    if (storedCity) {
+      this.city = storedCity;
+      this.getWeather();
+    }
+  }
+
+  async ionViewWillEnter() {
+    await this.storage.create();
+    const storedCity = await this.storage.get('city');
+    if (storedCity) {
+      this.city = storedCity;
+      this.getWeather();
+    }
   }
 
   getWeather(): void {
@@ -28,6 +44,11 @@ temperature: string = "";
       this.weatherData = data;
       this.temperature = data.main.temp.toFixed(2);
     });
+  }
+
+  async saveCity(): Promise<void> {
+    await this.storage.set('city', this.city);
+    console.log('City saved to storage:', this.city);
   }
 
   toggleDarkMode(event: any) {
